1950b9ad53f9114b32ed545f9c0c5d3ccff622c6
MUNUSAMYSUBBAIYA <ms105686@gmail.com>
2025-04-30 18:31:58 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,35 @@ app.get("/", (req, res) => {
   res.json({message: "Hello World!"});
 });
 
+
+// CRUD functionality of movies
+
+app.get("/movies", (req, res) => {
+
+});
+
+
+app.post("/movies", () => {
+
+});
+
+
+app.get("/movies/:id", () => {
+
+});
+
+
+app.put("/movies/:id", () => {
+
+});
+
+
+app.delete("/movies:id", () => {
+
+});
+
+
+
 let PORT = 3000;
 
 app.listen(PORT, () => {
