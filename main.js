35beb72d80eee4ef266728e53cf3354bd09d3571
MUNUSAMYSUBBAIYA <ms105686@gmail.com>
2025-05-01 11:46:28 +0530

    understanding middleware changes in main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import movieRouter from "./routes/movies.route.js";
 import connectDB from "./lib/db.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(urlencoded({extended: true}));
 
 // Connect DB
 connectDB();
